2beb43492759e396d320a1b5481fb6f7c095d1d5
HermonMartinez <42020540+HermonMartinez@users.noreply.github.com>
2018-08-11 17:12:17 -0600

    get and set

--- Bank_XYZ/src/logica/SexoLogica.java
+++ Bank_XYZ/src/logica/SexoLogica.java
@@ -10,5 +10,24 @@ package logica;
  * @author AG
  */
 public class SexoLogica {
-    
+     private int idSexo;
+    private String Sexo;
+
+    public int getIdSexo() {
+        return idSexo;
+    }
+
+    public void setIdSexo(int idSexo) {
+        this.idSexo = idSexo;
+    }
+
+    public String getSexo() {
+        return Sexo;
+    }
+
+    public void setSexo(String Sexo) {
+        this.Sexo = Sexo;
+    }
+
+   
 }
